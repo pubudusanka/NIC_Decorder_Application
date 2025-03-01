19a4c9a63e64da9137be6d11d04dddd5da8001df
Pubudu Sanka <pubudusanka79@gmail.com>
2025-03-01 22:37:55 +0530

    Create DetailRow.dart

---
+++ lib/DetailRow.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DetailRow extends StatelessWidget {
+  final String title;
+  final String value;
+
+  DetailRow({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            value,
+            style: TextStyle(fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+}
