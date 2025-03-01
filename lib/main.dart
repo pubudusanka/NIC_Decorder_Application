5608c88afb2db61a86d9d6fab43012269a11f189
Pubudu Sanka <pubudusanka79@gmail.com>
2025-03-01 22:37:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'screens/HomeScreen.dart';
+import 'controller/NicController.dart';
+import 'screens/ResultScreen.dart';
+import 'widgets/HeaderWidget.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +13,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sri Lanka NIC Decoder',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final NicController nicController = Get.put(NicController());
+  final TextEditingController nicInputController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          HeaderWidget(title: "SRI LANKA NIC DECORDER"),
+          SizedBox(height: 20),
+          Image.network(
+            'https://atlas-content-cdn.pixelsquid.com/stock-images/sri-lanka-desk-flag-XlEQWJ5-600.jpg',
+            height: 150,
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: nicInputController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter NIC Number',
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              nicController.decodeNIC(nicInputController.text);
+              if (nicController.isValidNIC.value) {
+                Get.to(ResultScreen());
+              }
+            },
+            child: Text("Decode"),
+          ),
+        ],
+      ),
     );
   }
 }
